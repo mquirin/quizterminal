1a3b6c66c7cd25186c59aa0e9cf633514eb03459
Michael Quirin <michael.e.quirin@gmail.com>
2014-05-09 20:51:41 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace FormTerminal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+           
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.text = new System.Windows.Forms.Label();
+            this.text_cursor = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.merowinger = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Font = new System.Drawing.Font("Consolas", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text.ForeColor = System.Drawing.Color.Lime;
+            this.text.Location = new System.Drawing.Point(39, 36);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(79, 43);
+            this.text.TabIndex = 0;
+            this.text.Text = ">: ";
+            // 
+            // text_cursor
+            // 
+            this.text_cursor.AutoSize = true;
+            this.text_cursor.Font = new System.Drawing.Font("Consolas", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_cursor.ForeColor = System.Drawing.Color.Lime;
+            this.text_cursor.Location = new System.Drawing.Point(39, 36);
+            this.text_cursor.Name = "text_cursor";
+            this.text_cursor.Size = new System.Drawing.Size(99, 43);
+            this.text_cursor.TabIndex = 1;
+            this.text_cursor.Text = ">: █";
+            this.text_cursor.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // merowinger
+            // 
+            this.merowinger.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.merowinger.AutoSize = true;
+            this.merowinger.Font = new System.Drawing.Font("Consolas", 13.94764F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.merowinger.ForeColor = System.Drawing.Color.Orange;
+            this.merowinger.Location = new System.Drawing.Point(889, 531);
+            this.merowinger.Name = "merowinger";
+            this.merowinger.Size = new System.Drawing.Size(39, 43);
+            this.merowinger.TabIndex = 2;
+            this.merowinger.Text = "π";
+            this.merowinger.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.merowinger.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(940, 583);
+            this.Controls.Add(this.merowinger);
+            this.Controls.Add(this.text_cursor);
+            this.Controls.Add(this.text);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Yellow;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.Label text_cursor;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label merowinger;
+        private System.Windows.Forms.Timer timer2;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace FormTerminal
+{
+    public partial class Form1 : Form
+    {
+        enum mode {
+            normal,
+            locked,
+            userinput
+        }
+
+        secretPhrases   phrases         = new secretPhrases("exit", "4 8 15 16 23 42");
+        int             countdown       = 0;
+        int             lockedSize      = 3;
+        string          stringInit      = ">: ";
+        int             lockCountdown   = 30;
+        string          positionMarker  = "█";
+        string          fileWinner      = "C:\\temp\\winner.txt";
+        string          fileConfig      = "C:\\temp\\terminal.cfg";
+        string          winner;
+        mode            mMode;
+
+        public Form1() {
+            WindowState = FormWindowState.Maximized;
+            Cursor.Hide();
+            InitializeComponent();
+        }
+
+        private void wrongCodeFound() {
+            text.Text           = "Wrong Code. Console is locked for " + lockCountdown + " seconds";
+            text_cursor.Text    = text.Text;
+            countdown           = lockCountdown;
+            timer2.Enabled      = true;
+            mMode               = mode.locked;
+        }
+
+        private void secretCodeFound() {
+            timer2.Enabled      = false;
+            text.Text           = "Input Correct. Type in your user-id: ";
+            text_cursor.Text    = text.Text + positionMarker;
+            lockedSize          = text.Text.Length;
+            mMode               = mode.userinput;
+        }
+
+        private void userFinished() {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(fileWinner, true);
+            file.WriteLine(DateTime.Now.ToString() + " | " + winner + "\n");
+            file.Close();
+
+            text.Text           = text_cursor.Text = "Input Correct. All praise for User " + winner;
+            text_cursor.Visible = false;
+            mMode               = mode.locked;
+            timer1.Enabled      = false;
+            timer2.Enabled      = false;
+        }
+
+        private void exitToExplorer() {
+            Process P = new Process();
+            P.StartInfo.FileName = "explorer.exe";
+            P.Start();
+
+            Close();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (mMode == mode.locked)
+                return;
+
+            string input = text.Text.Remove(0, lockedSize);
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (mMode == mode.normal)
+                {
+                    if (input.CompareTo(phrases.secretMessage) == 0)
+                    {
+                        secretCodeFound();
+                        return;
+                    }
+
+                    else if (input.CompareTo(phrases.exitExplorer) == 0)
+                        exitToExplorer();
+
+                    else
+                    {
+                        wrongCodeFound();
+                        return;
+                    }
+                }
+                else if (mode.userinput == mMode)
+                {
+                    winner = input;
+                    userFinished();
+                }
+            }
+
+            // letters
+            else if (e.KeyCode >= Keys.A && e.KeyCode <= Keys.Z)
+            {
+                if (e.Shift)
+                    text.Text += e.KeyCode.ToString().ToUpper();
+                else
+                    text.Text += e.KeyCode.ToString().ToLower();
+            }
+
+            // space key
+            else if (e.KeyCode == Keys.Space)
+            {
+                text.Text += " ";
+            }
+
+            // numbers
+            else if (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9)
+            {
+                text.Text += (int)(e.KeyCode - Keys.D0);
+            }
+
+            // numpad
+            else if (e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9)
+            {
+                text.Text += (int)(e.KeyCode - Keys.NumPad0);
+            }
+
+            // comma
+            else if (e.KeyCode >= Keys.Decimal)
+            {
+                text.Text += " ";
+            }
+
+            // backspace
+            else if (e.KeyCode == Keys.Back && text.Text.Length > lockedSize)
+            {
+                text.Text = text.Text.Remove(text.Text.Length - 1);
+            }
+
+            text_cursor.Text = text.Text + positionMarker;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            Focus();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (mMode != mode.locked)
+                text_cursor.Visible = !text_cursor.Visible;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (countdown > 0)
+            {
+                countdown--;
+                text.Text           = "Wrong Code. Console is locked for " + countdown + " seconds";
+                text_cursor.Text    = text.Text;
+            }
+            else
+            {
+                text.Text           = stringInit;
+                text_cursor.Text    = text.Text + "█";
+                timer1.Enabled      = true;
+                timer2.Enabled      = false;
+                mMode               = mode.normal;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            phrases.loadFromFile(fileConfig);
+        }
+    }
+
+    class secretPhrases
+    {
+        public string exitExplorer;
+        public string secretMessage;
+
+        public secretPhrases(string exitExplorer, string secretMessage)
+        {
+            this.exitExplorer = exitExplorer;
+            this.secretMessage = secretMessage;
+        }
+
+        public void loadFromFile(string filename)
+        {
+            try
+            {
+                System.IO.StreamReader cfgFile = new System.IO.StreamReader(filename);
+                while (!cfgFile.EndOfStream)
+                {
+                    string[] param = cfgFile.ReadLine().Split(':');
+                    if (param.Length <= 1)
+                    {
+                        continue;
+                    }
+
+                    switch (param[0])
+                    {
+                        case "exitKey":
+                            exitExplorer = param[1];
+                            break;
+                        case "secret":
+                            secretMessage = param[1];
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                cfgFile.Close();
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    };
+}
